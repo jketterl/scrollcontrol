bdd5668b2b0832776680e84d67c2a1b32f5bfb6c
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-11-10 23:36:13 +0000

    add a navigation menu

--- ws/js/app.js
+++ ws/js/app.js
@@ -8,13 +8,37 @@ var app = function(){
 
     var viewport = Ext.create('Ext.Viewport', {
         layout:'border',
-        items:[{
+        items:[Ext.create('Ext.tree.Panel', {
             region:'west',
             title:'Navigation',
             width:200,
             split:true,
-            collapsible:true
-        },Ext.create('Matrix.SequenceEditor', {
+            rootVisible:false,
+            collapsible:true,
+            store:Ext.create('Ext.data.TreeStore', {
+                fields:['text', 'leaf', 'handler'],
+                root:{
+                    expanded:true,
+                    children:[{
+                        text:'Direct Mode',
+                        leaf:true,
+                        handler:function(){
+                            console.info('direct mode');
+                        }
+                    },{
+                        text:'Stored Sequences',
+                        leaf:true
+                    }]
+                }
+            }),
+            listeners:{
+                itemclick:function(tree, record){
+                    var fn = record.get('handler');
+                    if (fn) fn();
+                }
+            }
+        }),
+        Ext.create('Matrix.SequenceEditor', {
             title:'Direct mode',
             region:'center'
         })]
