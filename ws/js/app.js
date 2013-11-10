64f3205e586e109a1d65e56d6559807319b17b89
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-11-10 22:44:49 +0000

    make class files possible

--- ws/js/app.js
+++ ws/js/app.js
@@ -1,49 +1,11 @@
-Ext.define('Matrix.SequenceEditor', {
-    extend:'Ext.form.Panel',
-    frame:true,
-    items:[{
-        xtype:'textfield',
-        name:'text',
-        fieldLabel:'Scroll text'
-    },{
-        xtype:'slider',
-        name:'speed',
-        fieldLabel:'Scroll speed',
-        width:400,
-        value:10,
-        maxValue:255
-    }],
-    dockedItems:[{
-        xtype:'toolbar',
-        dock:'top',
-        items:[{
-            xtype:'button',
-            text:'Send to display',
-            handler:function(){
-                var form = this.up('form');
-                form.submit();
-           }
-        }]
-    }],
-    listeners: {
-        afterRender: function(thisForm, options){
-            this.keyNav = Ext.create('Ext.util.KeyNav', this.el, {                    
-                enter: function(){
-                    this.submit();
-                },
-                scope: this
-            });
-        }
-    },
-    submit:function(){
-        Ext.Ajax.request({
-            url:'/direct',
-            jsonData:this.getForm().getValues()
-        });
-    }
-});
-
 var app = function(){
+    Ext.Loader.setConfig({
+        enabled:true,
+        paths:{
+            'Matrix':'js/matrix'
+        }
+    });
+
     var viewport = Ext.create('Ext.Viewport', {
         layout:'border',
         items:[{
