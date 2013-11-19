35ad79326e539f7456feac2f47f472084122ad28
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-11-19 20:44:27 +0000

    run on 4 displays

--- scroller/scroller.ino
+++ scroller/scroller.ino
@@ -16,7 +16,6 @@ Screen* screens;
 void setup () {
   SPIReader.begin();
   HT1632.begin(A2, A0, A1, A3, A4, A5);
-  HT1632.drawTarget(BUFFER_BOARD(1));
   
   screenCount = 1;
   screens = (Screen*) malloc(sizeof(Screen));
@@ -42,13 +41,16 @@ void loop () {
       wd = HT1632.getTextWidth(current.message, FONT_7X5_WIDTH, FONT_7X5_HEIGHT);
     }
 
-    if (++i > wd + OUT_SIZE) i = 0;
+    if (++i > wd + OUT_SIZE * 4) i = 0;
     
-    HT1632.clear();
-    HT1632.drawText(current.message, OUT_SIZE - i, 0, FONT_7X5, FONT_7X5_WIDTH, FONT_7X5_HEIGHT, FONT_7X5_STEP_GLYPH);
-    HT1632.render();
+    for (int k = 0; k < 4; k++) {
+      HT1632.drawTarget(BUFFER_BOARD(k + 1));
+      HT1632.clear();
+      HT1632.drawText(current.message, OUT_SIZE * (4 - k) - i , 0, FONT_7X5, FONT_7X5_WIDTH, FONT_7X5_HEIGHT, FONT_7X5_STEP_GLYPH);
+      HT1632.render();
+    }
     
-    delay(1000 / max(1, current.speed));
+    delay(100 / max(1, current.speed));
   }
   
   digitalWrite(ACT_LED, HIGH);
