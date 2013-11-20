c0f223d9be2e82b3ebd341061adac94d55a76902
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-11-20 22:57:58 +0000

    shutdown db before repair

--- dbrepair.sh
+++ dbrepair.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo /etc/init.d/mongodb stop
 sudo /opt/mongo/bin/mongod -f /etc/mongodb.conf --dbpath /var/lib/mongodb/ --repair
 sudo chown mongodb.mongodb /var/lib/mongodb/* -R
 sudo /etc/init.d/mongodb start
