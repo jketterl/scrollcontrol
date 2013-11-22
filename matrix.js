0c7290c779bb098c282d83dc9859c7d8c2640a7b
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-11-22 20:56:27 +0000

    new default animation

--- matrix.js
+++ matrix.js
@@ -6,20 +6,56 @@ var Matrix = module.exports = function(){
     me.stream = fs.createWriteStream('/dev/spidev0.0');
     me.displaySequence(new Sequence({steps:[{
         text:'scroll control enabled :-)',
-        speed:50
+        animations:[{
+            type:'SCROLL_IN',
+            direction:'RIGHT',
+            speed:50
+        },{
+            type:'SCROLL_OUT',
+            direction:'LEFT',
+            speed:50
+        }]
     },{
         text:'ready',
-        speed:10
+        animations:[{
+            type:'SCROLL_IN',
+            direction:'BOTTOM',
+            speed:1
+        }, {
+            type:'HOLD',
+            speed:30
+        },{
+            type:'SCROLL_OUT',
+            direction:'TOP',
+            speed:1
+        }]
     },{
         text:'waiting for input',
-        speed:10
+        animations:[{
+            type:'HOLD',
+            speed:50
+        }]
     }]}));
 };
 
 Matrix.prototype.displaySequence = function(sequence){
-    this.write(sequence.getBinary());
-}
+    var me = this,
+        buf = sequence.getBinary(),
+        chunkSize = 20;
 
-Matrix.prototype.write = function(text) {
-    this.stream.write(text); 
+    var write = function(start) {
+        var end = start + chunkSize;
+        var finished = false;
+        if (end > buf.length) {
+            end = buf.length;
+            finished = true;
+        }
+        var chunk = buf.slice(start, end);
+        me.stream.write(chunk);
+        if (!finished) setTimeout(function(){
+            write(end);
+        }, 10);
+    };
+
+    write(0);
 }
