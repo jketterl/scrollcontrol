23f970a180dd0a7f3241c479d06e658ff766762b
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-11-22 20:01:26 +0000

    implement all the different animations

--- scroller/src/sketch.ino
+++ scroller/src/sketch.ino
@@ -4,6 +4,10 @@
 #include <SPIRead.h>
 
 #define ACT_LED 6
+#define DISPLAY_WIDTH 32
+#define DISPLAY_COUNT 4
+#define TOTAL_WIDTH DISPLAY_WIDTH * DISPLAY_COUNT
+#define TOTAL_HEIGHT 8
 
 struct Coordinate {
   int x;
@@ -17,7 +21,7 @@ class Animation {
     boolean finished = false;
   public:
     Animation(byte direction, byte speed);
-    virtual Coordinate init();
+    virtual Coordinate init(int width);
     virtual Coordinate step(Coordinate start);
     virtual void sleep();
     boolean isFinished();
@@ -28,7 +32,7 @@ Animation::Animation(byte direction, byte speed) {
   this->speed = speed;
 };
 
-Coordinate Animation::init() {
+Coordinate Animation::init(int width) {
   finished = false;
   Coordinate c = {0,0};
   return c;
@@ -47,23 +51,35 @@ boolean Animation::isFinished(){
   return finished;
 }
 
-class ScrollAnimation : public Animation {
+class ScrollInAnimation : public Animation {
+  private:
+    int target;
   public:
-    ScrollAnimation(byte direction, byte speed) : Animation(direction, speed) {};
-    Coordinate init();
+    ScrollInAnimation(byte direction, byte speed) : Animation(direction, speed) {};
+    Coordinate init(int width);
     Coordinate step(Coordinate start);
 };
 
-Coordinate ScrollAnimation::step(Coordinate start) {
+Coordinate ScrollInAnimation::step(Coordinate start) {
   Coordinate result;
   result.x = start.x;
   result.y = start.y;
   switch (direction) {
     case 1:
       result.x--;
-      if (result.x == 0) {
-        finished = true;
-      }
+      if (result.x == target) finished = true;
+      break;
+    case 0:
+      result.x++;
+      if (result.x == target) finished = true;
+      break;
+    case 2:
+      result.y++;
+      if (result.y == target) finished = true;
+      break;
+    case 3:
+      result.y--;
+      if (result.y == target) finished = true;
       break;
     default:
       finished = true;
@@ -71,14 +87,107 @@ Coordinate ScrollAnimation::step(Coordinate start) {
   return result;
 }
 
-Coordinate ScrollAnimation::init() {
+Coordinate ScrollInAnimation::init(int width) {
   finished = false;
   Coordinate result = {0,0};
+  target = 0;
   switch (direction) {
+    case 0:
+      result.x = -width;
+      break;
     case 1:
-      result.x = 128;
+      result.x = TOTAL_WIDTH + 1;
+      break;
+    case 2:
+      result.y = -TOTAL_HEIGHT;
+      break;
+    case 3:
+      result.y = TOTAL_HEIGHT + 1;
+      break;
   }
   return result;
+}
+
+class ScrollOutAnimation : public Animation {
+  private:
+    int target;
+  public:
+    ScrollOutAnimation(byte direction, byte speed) : Animation(direction, speed) {};
+    Coordinate init(int width);
+    Coordinate step(Coordinate start);
+};
+
+Coordinate ScrollOutAnimation::step(Coordinate start) {
+  Coordinate result;
+  result.x = start.x;
+  result.y = start.y;
+  switch (direction) {
+    case 1:
+      result.x++;
+      if (result.x == target) finished = true;
+      break;
+    case 0:
+      result.x--;
+      if (result.x == target) finished = true;
+      break;
+    case 2:
+      result.y--;
+      if (result.y == target) finished = true;
+      break;
+    case 3:
+      result.y++;
+      if (result.y == target) finished = true;
+      break;
+    default:
+      finished = true;
+  }
+  return result;
+}
+
+Coordinate ScrollOutAnimation::init(int width) {
+  finished = false;
+  Coordinate result = {0,0};
+  target = 0;
+  switch (direction) {
+    case 0:
+      target = -width;
+      break;
+    case 1:
+      target = TOTAL_WIDTH + 1;
+      break;
+    case 2:
+      target = -TOTAL_HEIGHT;
+      break;
+    case 3:
+      target = TOTAL_HEIGHT + 1;
+      break;
+  }
+  return result;
+}
+
+class HoldAnimation : public Animation {
+  private:
+    int steps;
+  public:
+    HoldAnimation(byte direction, byte speed) : Animation(direction, speed) {};
+    Coordinate init(int width);
+    Coordinate step(Coordinate start);
+    void sleep();
+};
+
+Coordinate HoldAnimation::init(int width) {
+  steps = 0; finished = false;
+  Coordinate result = { 0, 0 };
+  return result;
+}
+
+Coordinate HoldAnimation::step(Coordinate start) {
+  if (++steps >= speed) finished = true;
+  return start;
+}
+
+void HoldAnimation::sleep() {
+  delay(100);
 }
 
 struct Screen {
@@ -100,11 +209,14 @@ void setup () {
   screens[0].speed = 10;
   screens[0].message = "booting...";
 
-  screens[0].animCount = 1;
-  screens[0].animations = (Animation**) malloc(sizeof(Animation*));
-  screens[0].animations[0] = new ScrollAnimation(1, 10);
+  screens[0].animCount = 3;
+  screens[0].animations = (Animation**) malloc(sizeof(Animation*) * 3);
+  screens[0].animations[0] = new ScrollInAnimation(3, 1);
+  screens[0].animations[1] = new HoldAnimation(0, 5);
+  screens[0].animations[2] = new ScrollOutAnimation(3, 10);
   
   pinMode(ACT_LED, OUTPUT);
+  digitalWrite(ACT_LED, HIGH);
 }
 
 int i = 0;
@@ -122,7 +234,7 @@ void loop () {
       pos %= screenCount;
       wd = HT1632.getTextWidth(current.message, FONT_7X5_WIDTH, FONT_7X5_HEIGHT);
       i = 0;
-      p = current.animations[i]->init();
+      p = current.animations[i]->init(wd);
     }
 
     p = current.animations[i]->step(p);
@@ -141,15 +253,18 @@ void loop () {
     if (current.animations[i]->isFinished()) {
       if (++i >= current.animCount) {
         i = -1;
-        digitalWrite(ACT_LED, HIGH);
+        //digitalWrite(ACT_LED, HIGH);
       } else {
-        p = current.animations[i]->init();
+        p = current.animations[i]->init(wd);
       }
     }
   }
   
   for (int k = 0; k < screenCount; k++) {
     free(screens[k].message);
+    for (int l = 0; l < screens[l].animCount; l++) {
+      free(screens[k].animations[l]);
+    }
     free(screens[k].animations);
   }
   free(screens);
@@ -172,7 +287,13 @@ void loop () {
         SPIReader.readBytes((char*) buf, 3);
         switch(buf[0]) {
           case 0:
-            screens[k].animations[l] = new ScrollAnimation(buf[1], buf[2]);
+            screens[k].animations[l] = new ScrollInAnimation(buf[1], buf[2]);
+            break;
+          case 1:
+            screens[k].animations[l] = new ScrollOutAnimation(buf[1], buf[2]);
+            break;
+          case 2:
+            screens[k].animations[l] = new HoldAnimation(buf[1], buf[2]);
             break;
           default:
             screens[k].animations[l] = new Animation(buf[1], buf[2]);
